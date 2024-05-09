8112b687e1bfa16549437c824a4ce0e9e5150a28
Franciélly Lima <49539428+franciellylima@users.noreply.github.com>
2024-05-09 19:54:01 +0000

    pages adicionada

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+//GIT INFO
+
+// APONTAMENTOS GIT BLOB
+
+function Home(){
+    return <h1></h1>
+}
+
+//GIT INFO
+
+// APONTAMENTOS GIT BLOB
+
+function Home(){
+    return <h1></h1>
+}
+
+export default Home;
